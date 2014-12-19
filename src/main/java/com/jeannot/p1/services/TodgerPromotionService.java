f67ffe500236baa260a0e2be5ceb91cd85ad3a80
Paul Milner <paul.milner@gmail.com>
2014-12-19 16:41:10 +0000

    Add a service
    Adding TodgerPromotionService to promote todgers...

---
+++ src/main/java/com/jeannot/p1/services/TodgerPromotionService.java
@@ -0,0 +1,9 @@
+package com.jeannot.p1.services;
+
+import com.jeannot.p1.dto.Todger;
+
+public interface TodgerPromotionService {
+    
+    Todger promoteTodger(Todger todger);
+
+}
--- src/test/java/com/jeannot/p1/dto/TodgerTest.java
+++ src/test/java/com/jeannot/p1/dto/TodgerTest.java
@@ -1,18 +1,50 @@
 package com.jeannot.p1.dto;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.jeannot.p1.exception.TodgerException;
+import com.jeannot.p1.services.TodgerPromotionService;
+import com.jeannot.p1.services.impl.SimpleTodgerPromotionService;
 
 public class TodgerTest {
     
+    private TodgerPromotionService todgerPromotionService;
+    
+    @Before
+    public void setup() throws Exception {
+        todgerPromotionService = new SimpleTodgerPromotionService();
+    }
+    
     @Test
-    public void successfulTodgerPromotion() {
+    public void successfulTodgerCreation() {
         String todgerName = "Alan";
         Todger todger = new Todger(todgerName,TodgerType.MINOR);
         assertEquals(todger.getName(),todgerName);
         assertTrue(todger.getTodgerType()==TodgerType.MINOR);
+    }
+
+    @Test
+    public void successfulTodgerPromotion() {
+        String todgerName = "Alan";
+        Todger todger1 = new Todger(todgerName,TodgerType.MINOR);
+        Todger promotedTodger1 = todgerPromotionService.promoteTodger(todger1);
+        assertTrue(promotedTodger1.getTodgerType()==TodgerType.MAJOR);
+        Todger promotedTodger2 = todgerPromotionService.promoteTodger(promotedTodger1);
+        assertTrue(promotedTodger2.getTodgerType()==TodgerType.SUPREME);
+        try {
+            Todger promotedTodger3 = todgerPromotionService.promoteTodger(promotedTodger2);
+            fail("An exception should have been thrown because the Todger cannot be promoted any further");
+        }
+        catch (TodgerException te) {
+            assertNotNull(te);
+        }
+        
     }
 
 }
