3d7a13cc47dcf31f94e57b63df845145234d0514
Paul Milner <paul.milner@gmail.com>
2014-12-24 11:49:24 +0000

    Update demo

--- src/main/java/com/jeannot/p1/App.java
+++ src/main/java/com/jeannot/p1/App.java
@@ -4,7 +4,9 @@ import com.jeannot.p1.dto.Todger;
 import com.jeannot.p1.dto.TodgerStatus;
 import com.jeannot.p1.dto.TodgerType;
 import com.jeannot.p1.services.TodgerPromotionService;
+import com.jeannot.p1.services.TodgerResignationService;
 import com.jeannot.p1.services.impl.SimpleTodgerPromotionService;
+import com.jeannot.p1.services.impl.SimpleTodgerResignationService;
 
 /**
  * Hello world!
@@ -23,6 +25,17 @@ public class App
         TodgerPromotionService todgerPromotionService = new SimpleTodgerPromotionService();
         Todger t2 = todgerPromotionService.promoteTodger(t);
         System.out.println(t2.toString());
+
+        TodgerResignationService todgerResignationService = new SimpleTodgerResignationService();
+        System.out.println("Resigning todger...");
+        Todger t3 = todgerResignationService.resign(t2);
+        System.out.println(t3.toString());
+
+        Todger t4 = new Todger("Andrew", TodgerType.MAJOR, TodgerStatus.THRUSTING);
+        System.out.println(t4.toString());
+        System.out.println("Firing todger...");
+        Todger t5 = todgerResignationService.fire(t4);
+        System.out.println(t5.toString());
 
         System.out.println( "Finished." );
     }
